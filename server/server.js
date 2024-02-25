ee1384efadfe3388c75d91b0c8aa3f300ad104c2
mhgarry <mhgarry92@gmail.com>
2024-02-25 16:04:08 -0500

    create basic apolloserver instance

--- server/server.js
+++ server/server.js
@@ -1,14 +1,27 @@
-const express = require("express");
-const PORT = process.env.PORT || 8888;
+import express from "express";
+import { ApolloServer } from "@apollo/server";
+import http from "http";
+import { expressMiddleware } from "@apollo/server/express4";
+import cors from "cors";
+import { ApolloServerPluginDrainHttpServer } from "@apollo/server/plugin/drainHttpServer";
+import { typeDefs } from "./typeDefs.mjs";
 
 const app = express();
-
-app.use(express.static("public"));
-
-app.get("/test", (req, res) => {
-  res.send("Hello World");
+const httpServer = http.createServer(app);
+const server = new ApolloServer({
+  typeDefs,
+  plugins: [ApolloServerPluginDrainHttpServer({ httpServer })],
 });
+await server.start();
 
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
-});
+app.use(
+  "/graphql",
+  cors(),
+  express.json(),
+  expressMiddleware(server, {
+    context: async ({ req }) => ({ token: req.headers.token }),
+  })
+);
+
+await new Promise((resolve) => httpServer.listen({ port: 8888 }, resolve));
+console.log(`Server is running on localhost:8888/graphql`);
